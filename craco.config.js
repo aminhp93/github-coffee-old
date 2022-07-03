d5b673cf786835c88c2d40d88aea5ae91092cbbb
Minh  Pham <minhpham@Minhs-MacBook-Pro.local>
2022-07-03 10:42:43 +0700

    Update

---
+++ craco.config.js
@@ -0,0 +1,45 @@
+const CracoLessPlugin = require('craco-less');
+// const path = require('path');
+// const CracoAlias = require("craco-alias");
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: { '@primary-color': '#1DA57A' },
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+    // {
+    //     plugin: CracoAlias,
+    //     options: {
+    //         source: "tsconfig",
+    //         // baseUrl SHOULD be specified
+    //         // plugin does not take it from tsconfig
+    //         baseUrl: "./src",
+    //         // tsConfigPath should point to the file where "baseUrl" and "paths" are specified
+    //         tsConfigPath: "./tsconfig.extend.json"
+    //     }
+    // }
+  ],
+  // webpack: {
+  //     configure: {
+  //         optimization: {
+  //             splitChunks: {
+  //                 chunks: 'async',
+  //                 maxSize: 5000000
+  //             },
+  //         },
+
+  //     },
+  //     output: {
+  //         filename: '[name].bundle.js'
+  //     },
+
+  // }
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,37 @@
-import { Counter } from "./features/counter/Counter";
-import "./App.css";
+import { Counter } from './features/counter/Counter';
 
-import { Plate, TEditableProps } from "@udecode/plate";
+import { Plate, TEditableProps } from '@udecode/plate';
+import { Button } from 'antd';
+import { Col, Row } from 'antd';
 
 const editableProps: TEditableProps = {
-  placeholder: "Type...",
+  placeholder: 'Type...',
 };
 
 function App() {
   return (
     <div className="App">
-      <Plate editableProps={editableProps} />
-
-      <Counter />
+      {/* <Plate editableProps={editableProps} /> */}
+      {/* <Counter /> */}
+      {/* <Button>Test</Button> */}
+      <Button type="primary">Primary Button</Button>
+      <Button>Default Button</Button>
+      <Button type="dashed">Dashed Button</Button>
+      <br />
+      <Button type="text">Text Button</Button>
+      <Button type="link">Link Button</Button>
+      <Row>
+        <Col xs={2} sm={4} md={6} lg={8} xl={10}>
+          Col
+        </Col>
+        <Col xs={20} sm={16} md={12} lg={8} xl={4}>
+          Col
+        </Col>
+        <Col xs={2} sm={4} md={6} lg={8} xl={10}>
+          Col
+        </Col>
+      </Row>
+      test 123
     </div>
   );
 }
