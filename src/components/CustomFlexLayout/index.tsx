c2e4cd7a5328403461fe2a0e6fd7d7722e04475a
Minh Pham <minhpham@Minhs-Mac-mini.localdomain>
2022-09-21 16:49:23 +0700

    Update flexlayout

--- src/components/CustomFlexLayout/index.tsx
+++ src/components/CustomFlexLayout/index.tsx
@@ -1,14 +1,31 @@
-import { IJsonModel, Layout, Model, TabNode } from 'flexlayout-react';
+import {
+  BorderNode,
+  IJsonModel,
+  ITabSetRenderValues,
+  Layout,
+  Model,
+  TabNode,
+  TabSetNode,
+} from 'flexlayout-react';
 import 'flexlayout-react/style/light.css';
 import './CustomFlexLayout.less';
 
+import { Button } from 'antd';
 import Chat from 'features/chat';
 import Post from 'features/post';
 import PostDetail from 'features/post/PostDetail';
+import Snippet from 'features/snippet';
 import Stock from 'features/stock';
 import StockMarketOverview from 'features/stock/StockMarketOverview';
 import StockNews from 'features/stock/StockNews';
 import StockTable from 'features/stock/StockTable';
+import * as React from 'react';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Box from '@mui/material/Box';
+import ConnectDashboard from 'features/connectDashboard';
 
 const DEFAULT_JSON: IJsonModel = {
   global: { tabEnableFloat: true },
@@ -51,6 +68,8 @@ const COMPONENT_OBJ: { [index: string]: any } = {
   Chat: <Chat hideOnlineUsers />,
   Todos: <PostDetail slug="todos" />,
   StockTable: <StockTable />,
+  Snippet: <Snippet />,
+  ConnectDashboard: <ConnectDashboard />,
 };
 interface IProps {
   json?: IJsonModel;
@@ -59,6 +78,16 @@ interface IProps {
 
 function FlexLayout({ json, onModelChange }: IProps) {
   const model = Model.fromJson(json || DEFAULT_JSON);
+  let layoutRef: React.RefObject<Layout> = React.createRef();
+  let nextGridIndex: number = 1;
+
+  const handleChange = (
+    event: SelectChangeEvent,
+    node: TabSetNode | BorderNode
+  ) => {
+    // setComponent(event.target.value as string);
+    onAddFromTabSetButton(node, event.target.value);
+  };
 
   const factory = (node: TabNode) => {
     console.log(node);
@@ -66,13 +95,87 @@ function FlexLayout({ json, onModelChange }: IProps) {
     return COMPONENT_OBJ[component];
   };
 
+  const onAddActiveClick = (event: React.MouseEvent) => {
+    layoutRef!.current!.addTabToActiveTabSet({
+      component: 'grid',
+      icon: 'images/article.svg',
+      name: 'Grid ' + nextGridIndex++,
+    });
+  };
+
+  const onRenderTabSet = (
+    node: TabSetNode | BorderNode,
+    renderValues: ITabSetRenderValues
+  ) => {
+    renderValues.stickyButtons.push(
+      <Box sx={{ minWidth: 120 }}>
+        <FormControl fullWidth>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            label="Component"
+            onChange={(e: any) => handleChange(e, node)}
+          >
+            {Object.values(COMPONENT_OBJ).map((component, index) => {
+              return (
+                <MenuItem value={Object.keys(COMPONENT_OBJ)[index]}>
+                  {Object.keys(COMPONENT_OBJ)[index]}
+                </MenuItem>
+              );
+            })}
+          </Select>
+        </FormControl>
+      </Box>
+      // <Button onClick={() => onAddFromTabSetButton(node, 'Snippet')}>+</Button>
+    );
+  };
+
+  const onAddFromTabSetButton = (
+    node: TabSetNode | BorderNode,
+    componentName?: string
+  ) => {
+    console.log(componentName);
+    layoutRef!.current!.addTabToTabSet(node.getId(), {
+      component: componentName,
+      name: componentName,
+    });
+  };
+
   return (
     <div className="CustomFlexLayout relative height-100 width-100">
+      <Button
+        style={{
+          background: 'red',
+          position: 'fixed',
+          top: 0,
+          right: 0,
+          zIndex: 1,
+        }}
+        onClick={onAddActiveClick}
+      >
+        Test add
+      </Button>
       <Layout
         model={model}
         factory={factory}
         // onAction={handleOnAction}
         onModelChange={onModelChange}
+        ref={layoutRef}
+        // font={{ size: this.state.fontSize }}
+        // onAction={this.onAction}
+        // onModelChange={this.onModelChange}
+        // titleFactory={this.titleFactory}
+        // iconFactory={this.iconFactory}
+        // onRenderTab={this.onRenderTab}
+        onRenderTabSet={onRenderTabSet}
+        // onRenderDragRect={this.onRenderDragRect}
+        // onRenderFloatingTabPlaceholder={this.state.layoutFile === "newfeatures" ? this.onRenderFloatingTabPlaceholder : undefined}
+        // onExternalDrag={this.onExternalDrag}
+        // realtimeResize={this.state.realtimeResize}
+        // onTabDrag={this.state.layoutFile === "newfeatures" ? this.onTabDrag : undefined}
+        // onContextMenu={this.state.layoutFile === "newfeatures" ? this.onContextMenu : undefined}
+        // onAuxMouseClick={this.state.layoutFile === "newfeatures" ? this.onAuxMouseClick : undefined}
+        // onTabSetPlaceHolder={this.onTabSetPlaceHolder}
       />
       ;
     </div>
---
+++ src/features/connectDashboard/index.tsx
@@ -0,0 +1,5 @@
+export interface IConnectDashboardProps {}
+
+export default function ConnectDashboard(props: IConnectDashboardProps) {
+  return <div>ConnectDashboard</div>;
+}
