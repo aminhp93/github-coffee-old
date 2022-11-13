913de345e7cf4cc14a2d5ed552cd43f10168cbd8
aminhp93 <minhpn.org.ec@gmail.com>
2022-11-13 15:01:47 +0700

    Update

--- src/libs/config/index.ts
+++ src/libs/config/index.ts
@@ -1,7 +1,6 @@
-import development from './development';
 import production from './production';
 
-let config = development;
+let config = production;
 
 if (process.env.NODE_ENV === 'production') {
   config = production;
